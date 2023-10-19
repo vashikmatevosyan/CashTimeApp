433f5cf6e9b381e75e2df95d916339f9cc0f24a0
vashikmatevosyan <vashik531@gmail.com>
2023-10-19 14:46:22 +0400

    aaa

---
+++ src/screens/CreateCv.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from "react-native";
+
+function CreateCv(props) {
+    return (
+        <View><Text>DSFB</Text></View>
+    );
+}
+
+export default CreateCv;
